29de88ad1f71a60346b3307b4f1a2c4ac3524f38
Samuel Benitez <samuelebenitez21@gmail.com>
2021-02-20 20:22:06 -0300

    trying deploy

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
   const responseType = `token`;
   const redirectUriDev = `http:%2F%2Flocalhost:3000%2Fdashboard`;
   const redirectUriProd = "http://misikit.samuelebenitez.vercel.app/dashboard";
+  ("https://accounts.spotify.com/es-ES/authorize?client_id=2cdf605804824d54a2aa9167bab2b9c4&redirect_uri=https:%2F%2Fmusikit.natasharios.vercel.app%2Fmusic&response_type=token&scope=user-library-modify user-follow-read playlist-read-private user-read-recently-played streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-library-read");
 
   // https:%2F%2Fmisikit.samuelebenitez.vercel.app/dashboard
 
@@ -22,7 +23,7 @@ export default function Home() {
 
         <a
           className={style.login_link}
-          href={`${baseUrl}?client_id=${clientId}&scope=user-top-read user-read-recently-played user-library-read user-follow-read&response_type=${responseType}&redirect_uri=${redirectUriProd}`}
+          href={`https://accounts.spotify.com/es-ES/authorize?client_id=${clientId}&redirect_uri=https:%2F%2Fmisikit.samuelebenitez.vercel.app%2Fdashboard&response_type=token&scope=user-library-modify user-follow-read playlist-read-private user-read-recently-played streaming user-read-email user-read-private user-read-playback-state user-modify-playback-state user-library-read`}
         >
           Touch me for login!
         </a>
